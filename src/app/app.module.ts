a7955c2bde00083e2b0cea361381abb381b7bcce
Jorge De Jesus Rivera Perez <i20163mc061@utez.edu.mx>
2021-10-30 10:14:13 -0500

    Added the type definitions from @types/facebook-js-sdk

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { AccountService } from '@app/_services';
+import { environment } from '@environments/environment';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -14,3 +17,37 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+export function appInitializer(accountService: AccountService) {
+  return () => new Promise(resolve => {
+      // wait for facebook sdk to initialize before starting the angular app
+      window['fbAsyncInit'] = function () {
+          FB.init({
+              appId: environment.facebookAppId,
+              cookie: true,
+              xfbml: true,
+              version: 'v8.0'
+          });
+
+          // auto authenticate with the api if already logged in with facebook
+          FB.getLoginStatus(({authResponse}) => {
+              if (authResponse) {
+                  accountService.apiAuthenticate(authResponse.accessToken)
+                      .subscribe()
+                      .add(resolve);
+              } else {
+                  resolve();
+              }
+          });
+      };
+
+      // load facebook sdk script
+      (function (d, s, id) {
+          var js, fjs = d.getElementsByTagName(s)[0];
+          if (d.getElementById(id)) { return; }
+          js = d.createElement(s); js.id = id;
+          js.src = "https://connect.facebook.net/en_US/sdk.js";
+          fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));    
+  });
+}
