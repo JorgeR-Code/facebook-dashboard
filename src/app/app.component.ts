f3b81cb6e1138a18e566d247cbdf7f56f4b6e87d
Jorge De Jesus Rivera Perez <i20163mc061@utez.edu.mx>
2021-11-02 10:15:59 -0600

    Deploy Heroku

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,10 @@
 import { Component } from '@angular/core';
-import { FacebookService, InitParams } from 'ngx-facebook';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-
-
 export class AppComponent {
   title = 'facebookDashboard';
-
-}
-
-
-export class MyComponentOrService {
- 
-  constructor(private fb: FacebookService) {
- 
-    const initParams: InitParams = {
-      appId: '666709170961617',
-      xfbml: true,
-      version: 'v2.8'
-    };
- 
-    fb.init(initParams);
-  }
- 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,14 @@
-import { FacebookModule } from 'ngx-facebook';
-
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
-
-
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    FacebookModule.forRoot()
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
