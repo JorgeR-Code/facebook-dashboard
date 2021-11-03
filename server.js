61757129bb45cd5db1e84651fd00b0d8bef35b24
Jorge De Jesus Rivera Perez <i20163mc061@utez.edu.mx>
2021-11-03 11:59:00 -0600

    the path on server.js changed

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const express = require('express');
 const app = express();
 
 // Serve static files
-app.use(express.static(__dirname + '/dist/facebook-dashboard'));
+app.use(express.static(__dirname + '/src/index.html'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/facebook-dashboard/index.html'));
+  res.sendFile(path.join(__dirname + '/src/index.html'));
 });
 
 // default Heroku port
